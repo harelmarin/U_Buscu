d726e5199b776c019a818fb227d3a506f2fe07f2
harelmarin <marin.harel@ynov.com>
2024-07-07 14:59:18 +0200

    Add : Js files for burger

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const burgerMenu = document.getElementById('menu-burger');
+    const sideMenu = document.getElementById('side-menu');
+    const closeBtn = document.getElementById('close-btn');
+
+    if (burgerMenu && sideMenu && closeBtn) {
+        burgerMenu.addEventListener('click', function() {
+            if (sideMenu.style.width === '40vw') {
+                sideMenu.style.width = '0';
+                burgerMenu.classList.remove('change');
+            } else {
+                sideMenu.style.width = '40vw';
+                burgerMenu.classList.add('change');
+            }
+        });
+
+        closeBtn.addEventListener('click', function() {
+            sideMenu.style.width = '0';
+            burgerMenu.classList.remove('change');
+        });
+    } else {
+        console.error('One or more elements not found: burgerMenu, sideMenu, or closeBtn');
+    }
+});
