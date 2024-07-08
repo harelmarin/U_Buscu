eeac6109348424b9b4575fc27d7f3f62441cf70b
harelmarin <145020856+harelmarin@users.noreply.github.com>
2024-07-08 12:31:18 +0200

    add : new colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
     if (burgerMenu && sideMenu && closeBtn) {
         burgerMenu.addEventListener('click', function() {
-            if (sideMenu.style.width === '30vw') {
+            if (sideMenu.style.width === '35vw') {
                 sideMenu.style.width = '0';
                 burgerMenu.classList.remove('change');
             } else {
-                sideMenu.style.width = '30vw';
+                sideMenu.style.width = '35vw';
                 burgerMenu.classList.add('change');
             }
         });
