5d8eaa10dbfc24300d8ce95e675d2c4fb2ac47b2
harelmarin <145020856+harelmarin@users.noreply.github.com>
2024-07-08 12:32:51 +0200

    Add : script for burger

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,26 +3,40 @@ document.addEventListener('DOMContentLoaded', function() {
     const sideMenu = document.getElementById('side-menu');
     const closeBtn = document.getElementById('close-btn');
 
+    function closeMenu() {
+        sideMenu.style.width = '0';
+        burgerMenu.classList.remove('change');
+    }
+
     if (burgerMenu && sideMenu && closeBtn) {
-        burgerMenu.addEventListener('click', function() {
+        burgerMenu.addEventListener('click', function(event) {
+            event.stopPropagation(); // Prevent the click from bubbling up to the document
             if (sideMenu.style.width === '35vw') {
-                sideMenu.style.width = '0';
-                burgerMenu.classList.remove('change');
+                closeMenu();
             } else {
                 sideMenu.style.width = '35vw';
                 burgerMenu.classList.add('change');
             }
         });
 
-        closeBtn.addEventListener('click', function() {
-            sideMenu.style.width = '0';
-            burgerMenu.classList.remove('change');
+        closeBtn.addEventListener('click', function(event) {
+            event.stopPropagation(); // Prevent the click from bubbling up to the document
+            closeMenu();
+        });
+
+        document.addEventListener('click', function(event) {
+            if (sideMenu.style.width === '35vw' && !sideMenu.contains(event.target) && !burgerMenu.contains(event.target)) {
+                closeMenu();
+            }
+        });
+
+        sideMenu.addEventListener('click', function(event) {
+            event.stopPropagation(); // Prevent the click inside the sideMenu from bubbling up to the document
         });
     } else {
         console.error('One or more elements not found: burgerMenu, sideMenu, or closeBtn');
     }
 });
-
 
 document.addEventListener('DOMContentLoaded', function() {
     const container = document.querySelector('body');
